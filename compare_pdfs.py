4323a9c57e9a65ef8f97eea782f2dd10ba610000
JeffKun1999 <jefferson.ruiz.pico@udla.edu.ec>
2025-12-08 19:47:07 -0500

    Cambbio de contraseña de aplicacion

--- compare_pdfs.py
+++ compare_pdfs.py
@@ -76,16 +76,15 @@ def main():
     ld_sdk_key = os.getenv('LD_SDK_KEY')
     if not ld_sdk_key:
         print("Advertencia: LD_SDK_KEY no configurada. Ejecutando sin Feature Flags.")
-        # Aquí decides si fallar o continuar por defecto.
-        # Por seguridad, asumiremos ejecución normal si no hay key, o return.
+        
     else:
         ldclient.set_config(Config(ld_sdk_key))
 
-    # Definir el contexto (quién está ejecutando esto, puede ser el nombre del servicio)
+    # Definir el contexto (quién está ejecutando el código)
     context = Context.builder('jenkins-pipeline-bot').name('Jenkins CI').build()
 
     # Verificar la Feature Flag
-    # Si la bandera 'enable-pdf-comparison' es False, no hacemos nada.
+    # Si la bandera 'enable-pdf-comparison' es False, no hace nada.
     # El tercer parámetro (False) es el valor por defecto si falla la conexión.
     feature_enabled = ldclient.get().variation("enable-pdf-comparison", context, False)
 
@@ -93,7 +92,7 @@ def main():
         print("--- FEATURE FLAG: La comparación de PDFs está DESACTIVADA en LaunchDarkly ---")
         print("Saltando ejecución de lógica principal.")
         ldclient.get().close()
-        return  # Salimos exitosamente pero sin hacer el trabajo
+        return  # Sale exitosamente pero sin hacer el trabajo
     
     print("--- FEATURE FLAG: Funcionalidad ACTIVA. Procediendo... ---")
         
@@ -136,7 +135,7 @@ def main():
         sys.exit(1)
  # Cerrar el cliente al finalizar correctamente
     if ld_sdk_key:
-        ldclient.get().close() # <--- CORRECCIÓN AQUÍ
+        ldclient.get().close() 
 
 if __name__ == "__main__":
     main()
